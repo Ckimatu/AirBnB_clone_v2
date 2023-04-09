01c0a1da6459d196b96616bb7d41080a64215aab
Caroline <carolinekimatu6@gmail.com>
2023-04-09 09:18:44 -0700

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['54.162.0.15', '54.237.104.124']
+env.hosts = ['54.152.134.92', '54.172.230.218']
 
 
 @runs_once
